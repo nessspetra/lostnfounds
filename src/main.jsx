33825e6dffbc8bebc1f32993db6af2974eb4033b
Gayus Jones Petra <jonespetra102@gmail.com>
2025-10-11 21:34:26 +0700

    dapid

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,18 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/font/bootstrap-icons.css";
+
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
+const basename =
+  window.location.hostname.includes("github.io") ? "/lostnfounds" : "/";
+
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <StrictMode>
-      <BrowserRouter basename="/lostnfounds">
+      <BrowserRouter basename={basename}>
         <App />
       </BrowserRouter>
     </StrictMode>
